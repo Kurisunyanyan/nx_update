99f4778ed83ebc9210fa21b71062574c25587051
liuxiaopeng <liuxiaopeng@efy-tech.com>
2024-07-21 14:20:24 +0800

    update execute_update.sh

--- execute_update.sh
+++ execute_update.sh
@@ -1,12 +1,8 @@
 #!/bin/zsh
 
-# 源文件夹
 SRC_DIR="/home/nvidia/nx_update"
-# 目标文件夹
 DEST_DIR="/home/nvidia"
-# 新的文件夹
-NEW_DIR="$SRC_DIR/nx_ros_repo"
-# 备份文件夹
+FIRMWARE_DIR="/home/nvidia/nx_update/firmware"
 BACKUP_CONFIG_DIR="$SRC_DIR/config"
 
 # 检查源文件夹是否存在
@@ -35,11 +31,11 @@ if [ ! -d "$NEW_FLIGHT_LOGS_DIR" ]; then
     mkdir "$NEW_FLIGHT_LOGS_DIR"
 fi
 
-# 解压当前文件夹内的 nx_ros_repo.zip 到 /home/nvidia/nx_update/nx_ros_repo
+# 解压当前文件夹内的 nx_ros_repo.zip 到 /home/nvidia/nx_update/firmware
 ZIP_FILE="./nx_ros_repo.zip"
 if [ -f "$ZIP_FILE" ]; then
-    echo "Unzipping $ZIP_FILE to $NEW_DIR..."
-    unzip -q -o "$ZIP_FILE" -d "$NEW_DIR"
+    echo "Unzipping $ZIP_FILE to $FIRMWARE_DIR..."
+    unzip -q -o "$ZIP_FILE" -d "$FIRMWARE_DIR"
 else
     echo "Error: $ZIP_FILE does not exist."
     exit 1
@@ -51,7 +47,7 @@ if [ ! -d "$BACKUP_CONFIG_DIR" ]; then
 fi
 
 # 备份 uav_id_config.yaml 文件到 /home/nvidia/nx_update/config/
-UAV_ID_CONFIG_FILE="$NEW_DIR/src/cyber/launch_config/config/uav_id_config.yaml"
+UAV_ID_CONFIG_FILE="$FIRMWARE_DIR/src/cyber/launch_config/config/uav_id_config.yaml"
 if [ -f "$UAV_ID_CONFIG_FILE" ]; then
     echo "Backing up $UAV_ID_CONFIG_FILE to $BACKUP_CONFIG_DIR..."
     cp "$UAV_ID_CONFIG_FILE" "$BACKUP_CONFIG_DIR/"
@@ -59,25 +55,47 @@ fi
 
 # 备份原有的 nx_ros_repo 文件夹
 CURRENT_TIME=$(date +"%Y%m%d_%H%M%S")
-if [ -d "$NEW_DIR" ]; then
-    echo "Backing up existing $NEW_DIR to ${NEW_DIR}_bak_$CURRENT_TIME..."
-    mv "$NEW_DIR" "${NEW_DIR}_bak_$CURRENT_TIME"
+if [ -d "$FIRMWARE_DIR" ]; then
+    echo "Backing up existing $FIRMWARE_DIR to ${FIRMWARE_DIR}_bak_$CURRENT_TIME..."
+    mv "$FIRMWARE_DIR" "${FIRMWARE_DIR}_bak_$CURRENT_TIME"
 fi
 
 
 
 # 复制备份的 uav_id_config.yaml 文件到新的 nx_ros_repo 目录
 BACKUP_UAV_ID_CONFIG_FILE="$BACKUP_CONFIG_DIR/uav_id_config.yaml"
-NEW_UAV_ID_CONFIG_DIR="$NEW_DIR/src/cyber/launch_config/config"
+NEW_UAV_ID_CONFIG_DIR="$FIRMWARE_DIR/src/cyber/launch_config/config"
 if [ -f "$BACKUP_UAV_ID_CONFIG_FILE" ]; then
     echo "Copying $BACKUP_UAV_ID_CONFIG_FILE to $NEW_UAV_ID_CONFIG_DIR..."
     cp "$BACKUP_UAV_ID_CONFIG_FILE" "$NEW_UAV_ID_CONFIG_DIR/"
 fi
 
-# 移动 /home/nvidia/nx_update 文件夹内的所有内容到 /home/nvidia/
-for file in "$SRC_DIR"/*; do
-    echo "Moving $file to $DEST_DIR..."
-    mv "$file" "$DEST_DIR"
-done
+# 复制 /home/nvidia/nx_update/script/ 的内容到 /home/nvidia/
+SCRIPT_DIR="$SRC_DIR/script"
+if [ -d "$SCRIPT_DIR" ]; then
+    echo "Copying contents of $SCRIPT_DIR to $DEST_DIR..."
+    cp -r "$SCRIPT_DIR"/* "$DEST_DIR"
+else
+    echo "Error: $SCRIPT_DIR does not exist."
+    exit 1
+fi
+
+# 复制 /home/nvidia/nx_update/firmware/nx_ros_repo 到 /home/nvidia
+FIRMWARE_DIR="$SRC_DIR/firmware/nx_ros_repo"
+if [ -d "$FIRMWARE_DIR" ]; then
+    echo "Copying $FIRMWARE_DIR to $DEST_DIR..."
+    cp -r "$FIRMWARE_DIR" "$DEST_DIR"
+else
+    echo "Error: $FIRMWARE_DIR does not exist."
+    exit 1
+fi
+
+# 复制备份的 uav_id_config.yaml 文件到新的 nx_ros_repo 目录
+BACKUP_UAV_ID_CONFIG_FILE="$BACKUP_CONFIG_DIR/uav_id_config.yaml"
+NEW_UAV_ID_CONFIG_DIR="$DEST_DIR/nx_ros_repo/src/cyber/launch_config/config"
+if [ -f "$BACKUP_UAV_ID_CONFIG_FILE" ]; then
+    echo "Copying $BACKUP_UAV_ID_CONFIG_FILE to $NEW_UAV_ID_CONFIG_DIR..."
+    cp "$BACKUP_UAV_ID_CONFIG_FILE" "$NEW_UAV_ID_CONFIG_DIR/"
+fi
 
 echo "Setup complete."
